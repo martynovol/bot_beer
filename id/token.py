66d5a9dfd79a9ce0d64c5c9b30ed50b9bb919001
martynovol <oleg.mart2017@gmail.com>
2024-04-12 13:54:40 +0500

    new_

---
+++ id/token.py
@@ -0,0 +1,5 @@
+#test 5032830137:AAFQ7ZD5ZvMz0trVYFUjDFROJ-zcOQ_-uaI
+#bot_bar 6904217970:AAGOoPF6_nHlYyoq094WaSBtoInsMd6KkNo
+
+TokenBot = "5032830137:AAFQ7ZD5ZvMz0trVYFUjDFROJ-zcOQ_-uaI"
+headers = {"Authorization": "Bearer 71c87787faad3451488b032f62f449297a4e2a60"}
